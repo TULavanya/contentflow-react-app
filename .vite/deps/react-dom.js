047293c44bb3a740fcc416aa7de415735ad1c199
TULavanya <lavanya.tu@contentstack.com>
2025-10-29 17:52:50 +0530

    Fix: Add package-lock.json and exclude .vite cache for proper deployment

--- .vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-DFBJ4V7Y.js";
-import "./chunk-EZDWAL4P.js";
-import "./chunk-EQCVQC35.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
